084b672280f16fdadc6af98283f70a50efe9a077
Michael <a26mike@gmail.com>
2020-09-02 10:27:08 -0400

    changed variable names

--- rentalcar.py
+++ rentalcar.py
@@ -1,44 +1,41 @@
 #python 3.8.2
 import sys
 
-# charges
+# charges varibles that can chanage with rate increases later
 budget_charge = 40.00
 daily_charge = 60.00
 weekly_charge = 190.00
 over_charge = 0.25 # Over Milage charges 
-mile_charge = 0 # intiial charge start
+
 #Mile limits
 daily_limit = 100 # Daily Milage Limit
-weekly_Limit = 900 # weekly Milage limit 
-extraMiles = 0 #intial extra Miles
+weekly_Limit = 900 # weekly Milage limit
 
-def daysRented(days): #work on this later
-    pass
+mile_charge = 0 # intiial varible as 0 to remove extra else statments
+extra_miles = 0  #intiial varible as 0 to remove extra else statments
 
 # get information from user ,rental type, odometer intial and final reading
 rentalCode = input("(B)udget, (D)aily, or (W)eekly rental?\n").upper()
 odoStart = int(input("Starting Odometer Reading:\n"))
 odoEnd = int(input('Ending Odometer Reading:\n'))
-# get total miles driven
-totalMiles = odoEnd - odoStart
+
+totalMiles = odoEnd - odoStart# get total miles driven
 
 # if Daily or budget  or  weekly is rental code rental period is days
 # base charge of rental is rental period * type of charge
-
 if rentalCode == 'B':
     rentalPeriod = int(input("Number of Days Rented:\n"))
     baseCharge = rentalPeriod * budget_charge
-    # budget rental  mileage charge: $0.25 for each mile driven
-    mile_charge = totalMiles * over_charge
+    mile_charge = totalMiles * over_charge     # budget rental  mileage charge:for each mile driven
 
 elif rentalCode == 'D':  # daily rental code
     rentalPeriod = int(input("Number of Days Rented:\n"))
     baseharge = rentalPeriod * daily_charge
     averageDayMiles = totalMiles / rentalPeriod
-    # daily rental if under 100 daily miles charge daily rate if above add on 25cents per mile
+    # daily rental if under 100 daily miles charge daily rate if above add on  extra mileage charge per mile
     if averageDayMiles >= daily_limit:
-        extraMiles = averageDayMiles - daily_limit
-        mile_charge = extraMiles * over_charge
+        extra_miles = averageDayMiles - daily_limit
+        mile_charge = extra_miles * over_charge
 
 elif rentalCode == 'W':
     # if Weekly is rental code rental period is number of weeks
@@ -47,7 +44,7 @@ elif rentalCode == 'W':
     averageWeekMiles = totalMiles / rentalPeriod
     # daily rental if under 900 weekly miles charge weekly rate if above add on 25cents per mile
     if averageWeekMiles >= weekly_Limit:
-        extraMiles = averageWeekMiles - weekly_Limit
+        extra_miles = averageWeekMiles - weekly_Limit
         mile_charge = over_charge * rentalPeriod
 
 
@@ -65,7 +62,7 @@ Miles Driven:       {totalMiles}
 Amount Due:         {format(ammount, '.2f')}
 """)
 
-# other option to print out as in the
+# other option to print out as in the example
 
 # # print off the rental summary.
 # print('Rental Summary')
---
+++ scratchpad.py
@@ -0,0 +1,10 @@
+
+age =12
+if age >= 6 and age <= 11:
+  print('primary school')
+
+elif age >= 12 and age <=18:
+  print('secondary school')
+  
+else:
+  print(age)
